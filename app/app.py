6c994efe6de703c44e431cd2b7f0e01779b2bb64
neigood <neifergonzalez@gmail.com>
2022-02-04 16:14:03 -0300

    implementando mensaje agradable al usuario con erro 404

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__)
 
@@ -16,14 +16,31 @@ def index():
     return render_template('index.html', data=data)
 
 
-@app.route('/contacto/<nombre>')
-def contacto(nombre):
+@app.route('/contacto/<nombre>/<int:edad>')
+def contacto(nombre, edad):
     data = {
         'titulo': 'contacto',
-        'nombre': nombre
+        'nombre': nombre,
+        'edad': edad,
     }
     return render_template('contacto.html', data=data)
 
 
+def query_string():
+    print(request)
+    print(request)
+    print(request.args)
+    print(request.args.get('param1'))
+    print(request.args.get('param2'))
+    return "ok"
+
+
+def pagina_no_encontrada(error):
+    # return render_template('404.html'), 404
+    return redirect(url_for('index'))
+
+
 if __name__ == '__main__':
+    app.add_url_rule('/query_string', view_func=query_string)
+    app.register_error_handler(404, pagina_no_encontrada)
     app.run(debug=True, port=5000)
