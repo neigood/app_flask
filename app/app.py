3a47df91a6c2900aad209a07ce3247a6e7b00fc6
neigood <neifergonzalez@gmail.com>
2022-02-04 15:47:17 -0300

    implementando carpeta static

--- app/app.py
+++ app/app.py
@@ -16,5 +16,14 @@ def index():
     return render_template('index.html', data=data)
 
 
+@app.route('/contacto/<nombre>')
+def contacto(nombre):
+    data = {
+        'titulo': 'contacto',
+        'nombre': nombre
+    }
+    return render_template('contacto.html', data=data)
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
